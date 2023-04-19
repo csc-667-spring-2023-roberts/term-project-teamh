3f064a2e6d838f643ca2c352635e9f96a0c2921b
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-04-19 10:00:10 -0700

    complted loggedInHome and loggedOutHome

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,72 +2,76 @@ const express = require("express");
 const router = express.Router();
 
 // middleware to test if authenticated
-function isAuthenticated (req, res, next) {
-  if (req.session.user) next()
-  else next('route')
+function isAuthenticated(req, res, next) {
+  if (req.session.user) next();
+  else next("route");
 }
 
 // a middleware function with no mount path. This code is executed for every request to the router
 router.use((request, response, next) => {
-  console.log('Time:', Date.now())
+  console.log("Time:", Date.now());
   if (!request.session.user) {
-    response.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    response.header('Expires', '-1');
-    response.header('Pragma', 'no-cache');
-  } 
-  next()
-})
-
-router.get("/", isAuthenticated, (request, response) => {
-
-  const name = "person";
-  response.render("home", {
-      title: "Hi World!",
-      user: request.session.user,
-      message: "Our first template.",
-  });
-
+    response.header(
+      "Cache-Control",
+      "private, no-cache, no-store, must-revalidate"
+    );
+    response.header("Expires", "-1");
+    response.header("Pragma", "no-cache");
+  }
+  next();
 });
 
-router.get('/', function (request, response) {
+router.get("/", isAuthenticated, (request, response) => {
   const name = "person";
-  response.render("loggedOutHome", {
+  response.render("home", {
+    title: "Hi World!",
+    user: request.session.user,
+    message: "Our first template.",
   });
-})
+});
 
-router.get('/login', function (request, response) {
-  response.render("login", {
-  });
-})
+router.get("/", function (request, response) {
+  response.render("loggedOutHome", {});
+});
 
-router.get('/signup', function (request, response) {
+router.get("/login", function (request, response) {
+  response.render("login", {});
+});
+
+router.get("/signup", function (request, response) {
   response.render("signup", {
     title: "Hi World!",
   });
-})
+});
 
-router.post('/login', express.urlencoded({ extended: false }), function (request, response) {
-  
-  request.session.regenerate(function (err) {
-    if (err) next(err)
+router.post(
+  "/login",
+  express.urlencoded({ extended: false }),
+  function (request, response) {
+    request.session.regenerate(function (err) {
+      if (err) next(err);
 
-    // store user information in session, typically a user id
-    request.session.user = request.body.user
+      // store user information in session, typically a user id
+      request.session.user = request.body.user;
 
-    // save the session before redirection to ensure page
-    // load does not happen before session is saved
-    request.session.save(function (err) {
-      if (err) return next(err)
-      response.redirect('/')
-    })
-  })
-})
+      // save the session before redirection to ensure page
+      // load does not happen before session is saved
+      request.session.save(function (err) {
+        if (err) return next(err);
+        response.redirect("/");
+      });
+    });
+  }
+);
 
 // Logout page
 router.get("/logout", (request, response) => {
-  response.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  response.header('Expires', '-1');
-  response.header('Pragma', 'no-cache');
+  response.header(
+    "Cache-Control",
+    "private, no-cache, no-store, must-revalidate"
+  );
+  response.header("Expires", "-1");
+  response.header("Pragma", "no-cache");
   request.session.destroy();
   response.render("logout", {
     title: "Logout",
