19ee19ffc54636b713f438a315bca048dd059012
elisachih <elisa9928@gmail.com>
2023-05-05 14:38:35 -0700

    small refactor

--- backend/sockets/init.js
+++ backend/sockets/init.js
@@ -15,104 +15,19 @@ const initSockets = (app, sessionMiddleware) => {
       console.log("user disconnected");
     });
     _socket.on("joinroom", (data) => {
-      let payload = JSON.parse(data);
-      console.log(payload);
-      console.log("----joinroom");
-      _socket.join(payload.room);
-      let message = {
-        data: 'Joined', user: payload.user, room: payload.room
-      }
-      io.in(payload.room).emit("chat", JSON.stringify(message));
+      handleJoinRoom(io, _socket, data);
     });    
     _socket.on("chat", (data) => {
-      let payload = JSON.parse(data);
-      console.log(payload);
-      console.log("chat");
-      updateRoomChat(payload.room, payload.user, payload.data);
-      console.log(_socket.rooms);
-      _socket.join(payload.room);
-      io.in(payload.room).emit("chat", data);
+      handleChat(io, _socket, data);
     });
     _socket.on("draw", (data) => {
-      let payload = JSON.parse(data);
-      console.log("draw");
-      let cards = getCards();
-
-      let room = getRoomByName(payload.room);
-      console.log(room.deck.length);
-
-      // let num = Math.floor(Math.random() * cards.length);
-      message = {
-        event: "draw",
-        data: room.deck[0],
-      };
-
-      let me = getPlayerByRoomAndName(payload.room, payload.user);
-      console.log(me.hands.length)
-      me.hands.push(room.deck[0]);
-      console.log(me.hands.length)
-
-      room.deck.splice(0, 1);
-
-      message = JSON.stringify(message);
-      console.log("message" + message);
-      io.in(_socket.id).emit("draw", message);
+      handleDrawCard(io, _socket, data);
     });
     _socket.on("candiscardcard", (data, callback) => {
-      let payload = JSON.parse(data);
-      console.log(payload);
-      let room = getRoomByName(payload.room);
-      console.log(room.discardcard);
-      let card = getCardsById(payload.cardid);
-      
-      if (card.color === room.discardcard.color || card.value === room.discardcard.value) {
-        callback({
-          status: "ok"
-        });
-      } else {
-        callback({
-          status: "no"
-        });         
-      }
+      handleCanDiscardCard(_socket, data, callback)
     });
-
     _socket.on("discardcard", (data) => {
-      let payload = JSON.parse(data);
-      console.log("discardcard");
-      console.log(payload);
-      var message = {
-        cardimg: payload.data.imgsrc,
-        cardid: payload.data.id,
-      };
-      let room = getRoomByName(payload.room);
-      let me = getPlayerByRoomAndName(payload.room, payload.user);
-      console.log(me.hands);
-      let index = me.hands.findIndex((h) =>
-      {
-        if (h.cardId === payload.data.id) {
-          return true;
-        }
-      });
-      console.log(index);
-      let dis = me.hands.splice(index, 1);
-      room.discardcard = dis[0];
-      console.log(dis);
-      console.log(me.hands.length);
-
-      message = JSON.stringify(message);
-      io.in(payload.room).emit("discardcard", message);
-
-      console.log(" next player: ");
-      let nextplayer = getNextPlayerByRoom(payload.room);
-      console.log(" next player: ");
-      console.log(nextplayer);
-      message = {
-        player: nextplayer.name,
-      };
-      message = JSON.stringify(message);
-      console.log("message" + message);
-      io.in(payload.room).emit("whosturn", message);
-        
+      handleDiscardCard(io, _socket, data);        
     });
   });
 
@@ -120,5 +35,108 @@ const initSockets = (app, sessionMiddleware) => {
 
   return server;
 };
+
+const handleChat = (io, socket, data) => {
+  let payload = JSON.parse(data);
+  console.log(payload);
+  console.log("chat");
+  updateRoomChat(payload.room, payload.user, payload.data);
+  console.log(socket.rooms);
+  socket.join(payload.room);
+  io.in(payload.room).emit("chat", data);
+}
+
+const handleJoinRoom = (io, socket, data) => {
+  let payload = JSON.parse(data);
+  console.log(payload);
+  console.log("----joinroom");
+  socket.join(payload.room);
+  let message = {
+    data: 'Joined', user: payload.user, room: payload.room
+  }
+  io.in(payload.room).emit("chat", JSON.stringify(message));
+}
+
+const handleDrawCard = (io, socket, data) => {
+  let payload = JSON.parse(data);
+  console.log("draw");
+  let cards = getCards();
+
+  let room = getRoomByName(payload.room);
+  console.log(room.deck.length);
+
+  // let num = Math.floor(Math.random() * cards.length);
+  message = {
+    event: "draw",
+    data: room.deck[0],
+  };
+
+  let me = getPlayerByRoomAndName(payload.room, payload.user);
+  console.log(me.hands.length)
+  me.hands.push(room.deck[0]);
+  console.log(me.hands.length)
+
+  room.deck.splice(0, 1);
+
+  message = JSON.stringify(message);
+  console.log("message" + message);
+  io.in(socket.id).emit("draw", message);
+}
+
+const handleDiscardCard = (io, socket, data) => {
+  let payload = JSON.parse(data);
+  console.log("discardcard");
+  console.log(payload);
+  var message = {
+    cardimg: payload.data.imgsrc,
+    cardid: payload.data.id,
+  };
+  let room = getRoomByName(payload.room);
+  let me = getPlayerByRoomAndName(payload.room, payload.user);
+  console.log(me.hands);
+  let index = me.hands.findIndex((h) =>
+  {
+    if (h.cardId === payload.data.id) {
+      return true;
+    }
+  });
+  console.log(index);
+  let dis = me.hands.splice(index, 1);
+  room.discardcard = dis[0];
+  console.log(dis);
+  console.log(me.hands.length);
+
+  message = JSON.stringify(message);
+  io.in(payload.room).emit("discardcard", message);
+
+  console.log(" next player: ");
+  let nextplayer = getNextPlayerByRoom(payload.room);
+  console.log(" next player: ");
+  console.log(nextplayer);
+  message = {
+    player: nextplayer.name,
+  };
+  message = JSON.stringify(message);
+  console.log("message" + message);
+  io.in(payload.room).emit("whosturn", message);
+}
+
+const handleCanDiscardCard = (socket, data, callback) => {
+  let payload = JSON.parse(data);
+  console.log(payload);
+  let room = getRoomByName(payload.room);
+  console.log(room.discardcard);
+  let card = getCardsById(payload.cardid);
+  
+  if (card.color === room.discardcard.color || card.value === room.discardcard.value) {
+    callback({
+      status: "ok"
+    });
+  } else {
+    callback({
+      status: "no"
+    });         
+  }
+}
 
 module.exports = initSockets;
